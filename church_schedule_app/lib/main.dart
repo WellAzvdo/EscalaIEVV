c7a1007c79b687a10980259ef24670339a86684e
Well Lopes <wellersonazvdo@gmail.com>
2024-12-12 19:35:18 -0300

    add feature device preview

--- church_schedule_app/lib/main.dart
+++ church_schedule_app/lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:church_schedule_app/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true, // Habilite o device preview
+      builder: (context) => MyApp(), // A aplicação principal
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Church Schedule App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MainScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(), // Substitua com sua tela principal
+      builder: DevicePreview.appBuilder, // Necessário para o DevicePreview
     );
   }
 }
